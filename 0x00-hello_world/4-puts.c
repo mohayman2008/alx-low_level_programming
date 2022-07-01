4d918df17a7f9f77584b77e3669a1a8a742a0776
Mohamed Ayman <mohayman2007@gmail.com>
2022-06-30 19:10:13 -0700

    "Project "0x00. C - Hello, World" mandatory tasks (Tasks 0 - 6)

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Prints a sentence to the stdout
+ *
+ * Return: 0 if no error
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Prints a sentence to the stdout
+ *
+ * Return: 0 if no error
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
