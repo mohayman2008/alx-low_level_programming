1506209274450fb72fefee6c4a270725bf8db6aa
Mohamed Ayman <mohayman2007@gmail.com>
2022-06-30 19:50:24 -0700

    Project 0x00 - Task 8 - UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Sends a sentence to stderr
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+      char * quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+      fwrite(quote, 60, sizeof(char), stderr);
+	return (1);
+}
