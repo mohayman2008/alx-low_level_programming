481f739fd5a0f7cd58ac2544f8e7a17c44bbf4ef
Mohamed Ayman <mohayman2007@gmail.com>
2022-07-06 11:49:04 -0700

    Project 0x02 - Task 16 - In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,17 +10,35 @@
 int main(void)
 {
 	const int COUNT = 98;
+	const unsigned long MAX_DIGITS = 1000000000000000000;
 	int i = 0;
-	unsigned long tmp1 = 1, tmp2 = 2;
-	unsigned long current = 0;
+	unsigned long tmp1_lsd = 1, tmp1_hsd = 0;
+	unsigned long tmp2_lsd = 2, tmp2_hsd = 0;
+	unsigned long current_lsd = 0, current_hsd = 0;
 
-	printf("%lu, %lu", tmp1, tmp2);
+	printf("%lu, %lu", tmp1_lsd, tmp2_lsd);
 	while (i < COUNT - 2)
 	{
-		current = tmp1 + tmp2;
-		printf(", %lu", current);
-		tmp1 = tmp2;
-		tmp2 = current;
+		current_lsd = tmp1_lsd % MAX_DIGITS + tmp2_lsd % MAX_DIGITS;
+
+		current_hsd = current_lsd / MAX_DIGITS;
+		current_hsd += tmp1_lsd / MAX_DIGITS + tmp2_lsd / MAX_DIGITS;
+		current_hsd += tmp1_hsd + tmp2_hsd;
+
+		current_lsd %= MAX_DIGITS;
+
+		tmp1_lsd = tmp2_lsd;
+		tmp1_hsd = tmp2_hsd;
+
+		tmp2_lsd = current_lsd;
+		tmp2_hsd = current_hsd;
+
+		printf(", ");
+		if (current_hsd > 0)
+			printf("%lu%lu", current_hsd, current_lsd);
+		else
+			printf("%lu", current_lsd);
+
 		i++;
 	}
 	printf("\n");
