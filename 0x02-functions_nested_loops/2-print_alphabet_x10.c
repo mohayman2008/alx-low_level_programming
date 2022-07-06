f506cf063e2ffdcd0ab2ad4d346db987dd039d32
Mohamed Ayman <mohayman2007@gmail.com>
2022-07-05 19:59:17 -0700

    Project 0x02 - Tasks 2 -> 5

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet - print the alphabet
+ * print_alphabet_x10 - print the alphabet
  *
  * Return: nothing
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int i = 0;
 	char letter;
