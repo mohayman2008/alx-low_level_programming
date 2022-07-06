215f1cb76cc42f1b55a43d5169b3266d9f016562
Mohamed Ayman <mohayman2007@gmail.com>
2022-07-05 19:02:44 -0700

    Project 0x02 - Task 1 - I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphapet: print the alphabet
+ *
+ * Return: nothing
+ */
+
+void print_alphabet (void)
+{
+	char i = 'a';
+	while (i < 'z')
+	{
+		_putchar('i');
+		i++;
+	}
+}
