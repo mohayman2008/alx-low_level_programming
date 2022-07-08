218743c352ad3d4e592124e26b18218ffe0c93fc
Mohamed Ayman <mohayman2007@gmail.com>
2022-07-07 19:32:33 -0700

    Project 0x04 - Task 12 - Numbers have life ...

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - print number using _putchar(char) recursivly
+ * @n: number to be printed
+ *
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	if ((n / 10) == 0)
+	/* single digit number */
+	{
+		_putchar(n + 48);
+		return;
+	}
+
+	print_number(n / 10);
+	_putchar(n % 10 + 48);
+}
