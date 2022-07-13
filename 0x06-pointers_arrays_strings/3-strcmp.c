c7db3a1973c3d428607f1b81671761bbce63a328
Mohamed Ayman <mohayman2007@gmail.com>
2022-07-13 18:01:51 +0000

    Project 0x06 - Tasks 0 --> 7

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,23 +2,6 @@
 #include "main.h"
 
 /**
- * _strlen - find the length of a given string
- * @s: pointer to char array
- *
- * Return: The length of string
- */
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*(s + len) != '\0')
-	{
-		len++;
-	}
-	return (len);
-}
-
-/**
  * _strcmp - compare two strings
  * @s1: first string
  * @s2: second string
@@ -29,19 +12,6 @@ int _strcmp(char *s1, char *s2)
 {
 	int x = 0;
 	int i = 0;
-	int len1 = _strlen(s1), len2 = _strlen(s2);
-
-	if (len1 == 0)
-	{
-		if (len2 == 0)
-			return (0);
-		else
-			return (-s2[0]);
-	}
-	else if (len2 == 0)
-		return (s1[0]);
-	else
-		return (s1[0] - s2[0]);
 
 	while (s1[i] != '\0')
 	{
