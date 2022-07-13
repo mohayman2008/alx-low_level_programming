544b979001f24e55c08b3c21b3e86b7a554107a9
Mohamed Ayman <mohayman2007@gmail.com>
2022-07-13 19:56:05 +0000

    Project 0x06 - Task 9 - Numbers have life; they're not just symbols on paper

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_number - print number using _putchar(char) recursivly
+ * @n: number to be printed
+ *
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+
+		if ((n / 10))
+			print_number(n / -10);
+		_putchar((n % 10) * -1 + 48);
+		return;
+	}
+
+	if ((n / 10) == 0)
+	/* single digit number */
+	{
+		_putchar(n + 48);
+		return;
+	}
+
+	print_number(n / 10);
+	_putchar(n % 10 + 48);
+}
