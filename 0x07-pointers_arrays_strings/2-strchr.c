05eba28a4785210404dec6cfd8a6c714a74bc838
Mohamed Ayman <mohayman2007@gmail.com>
2022-07-18 23:59:59 +0000

    Project 0x07 - Tasks 0 --> 8

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,7 +13,7 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i, len;
 
-	for (len = 0 ; s[i] ; len++)
+	for (len = 0 ; s[len] ; len++)
 		;
 
 	for (i = 0 ; i <= len ; i++)
--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,25 +16,18 @@ char *_strstr(char *haystack, char *needle)
 
 	for (str_idx = 0 ; haystack[str_idx] ; str_idx++)
 	{
-		matched = 0;
-		if (haystack[str_idx] == needle[0])
+		matched = 1;
+		for (i = str_idx, j = 0 ; haystack[i] && needle[j] ; i++, j++)
 		{
-			i = str_idx + 1;
-			matched = 1;
-
-			for (j = 1 ; needle[j] && haystack[i] ; i++, j++)
+			if (haystack[i] != needle[j])
 			{
-				if (haystack[i] != needle[j])
-				{
-					matched = 0;
-					break;
-				}
+				matched = 0;
+				break;
 			}
-
-			if (matched && !needle[j])
-				return (haystack + str_idx);
 		}
-	}
 
+		if (matched && !needle[j])
+			return (haystack + str_idx);
+	}
 	return (NULL);
 }
