564e462baeb19c274f225152e143f58b727f6c09
Mohamed Ayman <mohayman2007@gmail.com>
2022-09-30 16:18:28 +0000

    Project 0x17 - Task 11 - crackme5

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,105 +3,40 @@
 #include <string.h>
 
 /**
- * f1 - performs some calculations
+ * keygen - generate the password for arbitary user
  * @user: username string
  * @len: length of username string
- *
- * Return: the result of the calculations
+ * @hash: hash array
+ * @pass: string array of length > 6 to hold the generated password
  */
-int f1(char *user __attribute__((unused)), int len)
-{
-	return ((len ^ 0x3b) & 0x3f);
-}
-
-/**
- * f2 - performs some calculations
- * @user: username string
- * @len: length of username string
- *
- * Return: the result of the calculations
- */
-int f2(char *user, int len)
+void keygen(char *user, int len, char *hash, char *pass)
 {
 	int i = 0, result = 0;
 
-	for (; i < len ; i++)
-	{
+	pass[0] = hash[(len ^ 0x3b) & 0x3f];
+
+	for (i = 0, result = 0; i < len ; i++)
 		result += user[i];
-	}
+	pass[1] = hash[(result ^ 0x4f) & 0x3f];
 
-	return ((result ^ 0x4f) & 0x3f);
-}
-
-/**
- * f3 - performs some calculations
- * @user: username string
- * @len: length of username string
- *
- * Return: the result of the calculations
- */
-int f3(char *user, int len)
-{
-	int i = 0, result = 1;
-
-	for (; i < len ; i++)
+	for (i = 0, result = 1; i < len ; i++)
 		result *= user[i];
+	pass[2] = hash[(result ^ 0x55) & 0x3f];
 
-	return ((result ^ 0x55) & 0x3f);
-}
-
-/**
- * f4 - performs some calculations
- * @user: username string
- * @len: length of username string
- *
- * Return: the result of the calculations
- */
-int f4(char *user, int len)
-{
-	int i = 0, result = *user;
-
-	for (; i < len ; i++)
+	for (i = 0, result = *user ; i < len ; i++)
 		if (user[i] > result)
 			result = user[i];
 	srand(result ^ 0xe);
-	return (rand() & 0x3f);
-}
+	pass[3] = hash[rand() & 0x3f];
 
-/**
- * f5 - performs some calculations
- * @user: username string
- * @len: length of username string
- *
- * Return: the result of the calculations
- */
-int f5(char *user, int len)
-{
-	int i = 0, result = 0;
-
-	for (; i < len ; i++)
-	{
+	for (i = 0, result = 0 ; i < len ; i++)
 		result += user[i] * user[i];
-	}
+	pass[4] = hash[(result ^ 0xef) & 0x3f];
 
-	return ((result ^ 0xef) & 0x3f);
-}
-
-/**
- * f6 - performs some calculations
- * @user: username string
- * @len: length of username string
- *
- * Return: the result of the calculations
- */
-int f6(char *user, int len __attribute__((unused)))
-{
-	int i = 0, result = 0;
-
-	for (; user[0] > i; i++)
+	for (i = 0, result = 0 ; user[0] > i; i++)
 		result = rand();
 
-	return ((result ^ 0xe5) & 0x3f);
+	pass[5] = hash[(result ^ 0xe5) & 0x3f];
 }
 
 /**
@@ -113,13 +48,12 @@ int f6(char *user, int len __attribute__((unused)))
  */
 int main(int ac, char **av)
 {
-        char *user, pass[7], *hash;
-	int len, i;
+	char *user, pass[7], *hash;
+	int len;
 	unsigned long int x[] = {0x3877445248432d41, 0x42394530534e6c37,
 				0x4d6e706762695432, 0x74767a5835737956,
 				0x2b554c59634a474f, 0x71786636576a6d34,
 				0x723161513346655a, 0x6b756f494b646850};
-	int (*func[])(char *, int) = {f1, f2, f3, f4, f5, f6};
 
 	if (ac != 2)
 	{
@@ -130,10 +64,9 @@ int main(int ac, char **av)
 	user = av[1];
 	len = strlen(user);
 	pass[6] = 0;
-
 	hash = (char *) &x;
-	for (i = 0 ; i < 6 ; i++)
-		pass[i] = hash[func[i](user, len)];
+
+	keygen(user, len, hash, pass);
 
 	printf("%s", pass);
 
