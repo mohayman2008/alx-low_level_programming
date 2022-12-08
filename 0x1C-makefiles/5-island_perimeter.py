2b6d12da2f52d5527c85560aa477cf9c69afd7c9
Mohamed Ayman <mohayman2007@gmail.com>
2022-12-08 02:07:04 +0000

    Project 0x1C - Mandatory task 5 - "Island Perimeter"

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,19 +7,18 @@ def island_perimeter(grid):
     if len(grid) < 1:
         return 0
     rows = 0
-    cols = 0
     row_counter = [0 for i in range(len(grid[0]))]
 
-    for row in grid:
+    for row in grid[1:-1]:
         not_empty = 0
-        j = 0
-        for cell in row:
+        j = 1
+        for cell in row[1:-1]:
             if cell == 1:
                 row_counter[j] = 1
                 not_empty = 1
             j += 1
         if not_empty:
-            cols += 1
-        elif cols > 0:
+            rows += 1
+        elif rows > 0:
             break
-    return 2 * cols + 2 * row_counter.count(1)
+    return 2 * rows + 2 * row_counter.count(1)
