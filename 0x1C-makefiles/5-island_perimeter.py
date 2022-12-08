4d0916675452e088700053c079d262fa03aeed7e
Mohamed Ayman <mohayman2007@gmail.com>
2022-12-08 00:52:43 +0000

    Project 0x1C - Mandatory task 5 - "Island Perimeter"

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module can be used to find the perimeter of an island"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid"""
+    if len(grid) < 1:
+        return 0
+    rows = 0
+    cols = 0
+    row_counter = [0 for i in range(len(grid[0]))]
+
+    for row in grid:
+        not_empty = 0
+        j = 0
+        for cell in row:
+            if cell == 1:
+                row_counter[j] = 1
+                not_empty = 1
+            j += 1
+        if not_empty:
+            cols += 1
+        elif cols > 0:
+            break
+    return 2 * cols + 2 * row_counter.count(1)
