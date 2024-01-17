96727b94798b4af524b928f3a78704ac65b7abbe
Mohamed Ayman <mohayman2007@gmail.com>
2024-01-17 02:17:20 +0000

    0x1E. C (Search Algorithms) - Advanced Tasks - 10 & 11

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -70,21 +70,3 @@ int exponential_search(int *array, size_t size, int value)
 		return (-1);
 	return (start + result);
 }
-
-/**
- * main - Entry point
- *
- * Return: Always EXIT_SUCCESS
- */
-int main(void)
-{
-	int array[] = {
-		0, 1, 2, 3, 4, 7, 12, 15, 18, 19, 23, 54, 61, 62, 76, 99
-	};
-	size_t size = sizeof(array) / sizeof(array[0]);
-
-	printf("Found %d at index: %d\n\n", 62, exponential_search(array, size, 62));
-	printf("Found %d at index: %d\n\n", 3, exponential_search(array, size, 3));
-	printf("Found %d at index: %d\n", 999, exponential_search(array, size, 999));
-	return (EXIT_SUCCESS);
-}
